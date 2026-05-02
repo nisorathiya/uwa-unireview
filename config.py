57f9d6da7ea67f92e4dc2effce32870ec9affe34
nxherl <nevisherl@gmail.com>
2026-05-02 16:56:11 +0800

    remove redundant commented-out database config lines

--- config.py
+++ config.py
@@ -8,7 +8,3 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///unireview.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # Database will be configured here in a later sprint
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///unireview.db'
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
